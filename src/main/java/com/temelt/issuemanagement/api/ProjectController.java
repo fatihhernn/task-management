c711855ea56f51947b00adb358bc8a7ea4852ce6
fatih eren <fatihhernn@gmail.com>
2020-12-22 23:13:00 +0300

    Pagination APIs

--- src/main/java/com/temelt/issuemanagement/api/ProjectController.java
+++ src/main/java/com/temelt/issuemanagement/api/ProjectController.java
@@ -3,8 +3,11 @@ package com.temelt.issuemanagement.api;
 import com.temelt.issuemanagement.dto.ProjectDto;
 import com.temelt.issuemanagement.service.implementation.ProjectServiceImp;
 import com.temelt.issuemanagement.util.ApiPaths;
+import com.temelt.issuemanagement.util.TPage;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +17,7 @@ import javax.validation.Valid;
 @RestController
 @RequestMapping(ApiPaths.ProjectCtrl.CTRL)
 @Api(value = "Project APIs")
+@Slf4j
 public class ProjectController {
 
     private final ProjectServiceImp projectServiceImp;
@@ -22,9 +26,19 @@ public class ProjectController {
         this.projectServiceImp=projectServiceImp;
     }
 
+
+    @GetMapping("/pagination")
+    @ApiOperation(value = "Get By Patination Operation",response = ProjectDto.class)
+    public ResponseEntity<TPage<ProjectDto>> getAllByPagination(Pageable pageable){
+        return ResponseEntity.ok(projectServiceImp.getAllPageable(pageable));
+    }
+
+
     @GetMapping("/{id}")
     @ApiOperation(value = "Get By ID Operation",response = ProjectDto.class)
     public ResponseEntity<ProjectDto> getById(@PathVariable("id") Long id){
+        log.info("ProjectController -> GetById -> PARAM : "+id);
+        log.debug("ProjectController -> GetById -> PARAM : "+id);
         ProjectDto projectDto= projectServiceImp.getById(id);
         return ResponseEntity.ok(projectDto);
     }
